f871145643c3a49eafd54b2130a60b1788d9f8e3
Jesse Whitham <jesse.whitham@gmail.com>
2019-01-29 12:07:11 +1300

    Update metadata with new info

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,16 @@
 name             'wordpress_lamp'
 maintainer       'Jesse Whitham'
 maintainer_email 'jesse.whitham@gmail.com'
-license          'GNU GPL v3'
+license          'GPL-3.0'
 description      'Installs/Configures wordpress via lamp server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
+
+source_url 'https://github.com/whithajess/wordpress_lamp'
+issues_url 'https://github.com/whithajess/wordpress_lamp/issues'
 
 supports 'amazon'
 
 depends 'lamp'
 
-source_url 'https://github.com/whithajess/wordpress_lamp'
-issues_url 'https://github.com/whithajess/wordpress_lamp/issues'
+chef_version '>= 12.10.24'
